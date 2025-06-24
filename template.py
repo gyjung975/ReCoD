551bbbfa9e271611553e92878ca1bc0c904b1af4
gyjung975 <gyjung9795@gmail.com>
2025-06-24 19:13:28 +0900

    first commit

---
+++ template.py
@@ -0,0 +1,12 @@
+caption_based = 'Reference descriptions:\n' \
+                '"""\n' \
+                '[txt]\n' \
+                '"""\n' \
+                'Describe this image by thinking about how it is different from the descriptions above,' \
+                ' without mentioning the difference, but giving details only about the image.'
+
+image_based = "Describe the first image by thinking about how it is different from the second image, " \
+              "without mentioning the difference, but giving details only about the first image."
+
+llava = 'Describe the image in great detail.\n'
+claude = 'Describe the image in great detail.'

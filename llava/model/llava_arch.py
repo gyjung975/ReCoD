eba1d529210317cc0e60052bf3fa495c3f5b1f84
gyjung975 <gyjung9795@gmail.com>
2026-02-11 20:04:17 +0900

    Update README.md

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -139,6 +139,8 @@ class LlavaMetaForCausalLM(ABC):
 
     def encode_images(self, images):
         image_features = self.get_model().get_vision_tower()(images)
+        if torch.cuda.device_count() == 1:
+            image_features = image_features.to('cuda:0')
         image_features = self.get_model().mm_projector(image_features)
         return image_features
 
